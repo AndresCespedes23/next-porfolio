e69b51b2b1fb798f5630f5c80f9f10382e0e0df1
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-03-29 14:37:50 -0300

    create home page

---
+++ src/Components/Header/index.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from "../../styles/header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.rocket}>👨‍🚀</div>
+      <nav>
+        <ul>
+          <Link className={styles.navLinks} href="/">
+            Home
+          </Link>
+          <Link className={styles.navLinks} href="/portfolio">
+            Portfolio
+          </Link>
+          <Link className={styles.navLinks} href="/contact">
+            Contact
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
