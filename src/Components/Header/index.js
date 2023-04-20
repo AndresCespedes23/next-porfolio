66011e3527cf0b92b00f125e3c1b9f467ae46a51
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-04-20 19:02:50 -0300

    add new logo

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/images/svg/sun.png";
 import styles from "../../styles/header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <Image className={styles.logo} src={logo} alt="logo" />
       <nav>
         <ul>
           <Link className={styles.navLinks} href="/">
@@ -17,6 +20,7 @@ export default function Header() {
           </Link>
         </ul>
       </nav>
+      <Image className={styles.logo} src={logo} alt="logo" />
     </header>
   );
 }
